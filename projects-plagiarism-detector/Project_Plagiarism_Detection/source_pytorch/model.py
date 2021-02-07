4750630647810803f69c105e0613baf5ce2d4817
Dhwani Bosamiya <20094625+Dhwanibosamiya@users.noreply.github.com>
2021-02-07 11:45:35 -0800

    UMLE-03 Model files

--- projects-plagiarism-detector/Project_Plagiarism_Detection/source_pytorch/model.py
+++ projects-plagiarism-detector/Project_Plagiarism_Detection/source_pytorch/model.py
@@ -1,67 +1,51 @@
-import torch
-import torch.nn as nn
+# torch imports
 import torch.nn.functional as F
+import torch.nn as nn
 
 
-## DONE: Complete this classifier
+## TODO: Complete this classifier
 class BinaryClassifier(nn.Module):
     """
     Define a neural network that performs binary classification.
     The network should accept your number of features as input, and produce 
     a single sigmoid value, that can be rounded to a label: 0 or 1, as output.
-
+    
     Notes on training:
     To train a binary classifier in PyTorch, use BCELoss.
-    BCELoss is binary cross entropy loss, documentation:
-    https://pytorch.org/docs/stable/nn.html#torch.nn.BCELoss
+    BCELoss is binary cross entropy loss, documentation: https://pytorch.org/docs/stable/nn.html#torch.nn.BCELoss
     """
 
-    ## DONE: Define the init function, the input params are required
-    # (for loading code in train.py to work)
-    def __init__(self, input_features, hidden_dim, output_dim, dropout=0.2):
+    ## TODO: Define the init function, the input params are required (for loading code in train.py to work)
+    def __init__(self, input_features, hidden_dim, output_dim):
         """
         Initialize the model by setting up linear layers.
         Use the input parameters to help define the layers of your model.
-
-        Arguments:
-        :param input_features: the number of input features in your \
-            training/test data
-        :param hidden_dim: define the number of nodes in the hidden layer(s)
+        :param input_features: the number of input features in your training/test data
+        :param hidden_dim: helps define the number of nodes in the hidden layer(s)
         :param output_dim: the number of outputs you want to produce
-        :param dropout: probability of an element to be zeroed. Default: 0.2
         """
         super(BinaryClassifier, self).__init__()
 
-        nodes = []
-        nodes.append(input_features)
-        if type(hidden_dim) == int:
-            nodes.append(hidden_dim)
-        elif type(hidden_dim) == list:
-            nodes.extend(hidden_dim)
-        nodes.append(output_dim)
+        # define any initial layers, here
+        # defining 2 linear layers
+        self.fc1 = nn.Linear(input_features, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.drop = nn.Dropout(0.3)
+        # sigmoid layer
+        self.sig = nn.Sigmoid()
+        
 
-        self.module_list = nn.ModuleList()
-        for n_in, n_out in zip(nodes[:-1], nodes[1:]):
-            self.module_list.append(nn.Linear(n_in, n_out))
-
-        self.dropout = nn.Dropout(dropout)
-
-    ## DONE: Define the feedforward behavior of the network
+    
+    ## TODO: Define the feedforward behavior of the network
     def forward(self, x):
         """
         Perform a forward pass of our model on input features, x.
-
-        Arguments:
-        :param x: A batch of input features of size (batch_size,input_features)
-
-        Return:
+        :param x: A batch of input features of size (batch_size, input_features)
         :return: A single, sigmoid-activated value as output
         """
-
+        
         # define the feedforward behavior
-        for layer in self.module_list[:-1]:
-            x = F.relu(layer(x))
-            x = self.dropout(x)
-        x = self.module_list[-1](x)
-
-        return torch.sigmoid(x)
+        out = F.relu(self.fc1(x)) # activation on hidden layer
+        out = self.drop(out)
+        out = self.fc2(out)
+        return self.sig(out) # returning class score
